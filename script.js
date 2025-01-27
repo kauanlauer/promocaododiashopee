0d3ff06e059af948611d61478052c05ddd511375
Kauan Lauer <143208136+kauanlauer@users.noreply.github.com>
2025-01-27 15:16:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 const productList = document.getElementById("product-list");
 const modalImage = document.getElementById("modalImage");
 
+// Função para embaralhar arrays
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 // Função para carregar os produtos
 function loadProducts() {
+  shuffleArray(products);
+
   products.forEach((product) => {
     const productElement = document.createElement("div");
     productElement.className = "col-12 col-sm-6 col-md-4 col-lg-3 mb-4";
@@ -23,12 +33,38 @@ function loadProducts() {
   });
 }
 
-// Função para visualizar a imagem em um modal
+// Função para carregar as lojas
+function loadStores() {
+  shuffleArray(stores);
+  const storeList = document.getElementById("store-list");
+
+  stores.forEach((store) => {
+    const storeElement = document.createElement("div");
+    storeElement.className = "col-12 col-sm-6 col-md-4 col-lg-3 mb-4";
+
+    storeElement.innerHTML = `
+      <div class="card h-100">
+        <img src="${store.image}" class="card-img-top" alt="${store.name}">
+        <div class="card-body text-center">
+          <h5 class="card-title">${store.name}</h5>
+          <a href="${store.url}" target="_blank" class="btn btn-danger w-100">Visitar Loja</a>
+        </div>
+      </div>
+    `;
+
+    storeList.appendChild(storeElement);
+  });
+}
+
+// Função para visualizar a imagem no modal
 function viewImage(imageUrl) {
   modalImage.src = imageUrl;
   const imageModal = new bootstrap.Modal(document.getElementById("imageModal"));
   imageModal.show();
 }
 
-// Carregar os produtos ao carregar a página
-document.addEventListener("DOMContentLoaded", loadProducts);
+// Carregar os produtos e lojas ao carregar a página
+document.addEventListener("DOMContentLoaded", () => {
+  loadProducts();
+  loadStores();
+});
---
+++ store.js
@@ -0,0 +1,24 @@
+// Lista de lojas mais bem avaliadas
+const stores = [
+    {
+      name: "Selaria Texas Country",
+      url: "https://s.shopee.com.br/qUBw9dc2Q",
+      image: "https://cf.shopee.sg/file/69f8a9d32b5cd9e5880e19bbaf98a528",
+    },
+    {
+      name: "GALAXIA PET BRASIL",
+      url: "https://s.shopee.com.br/7AOFTnpwAK",
+      image: "https://cf.shopee.sg/file/sg-11134004-7qvdf-lgy1yebl3c4jea",
+    },
+    {
+      name: "Marimport´s",
+      url: "https://s.shopee.com.br/6V8YgazdJu",
+      image: "https://cf.shopee.sg/file/cca6106a806ca2c085445b985bce257b",
+    },
+    {
+      name: "Magic Girassol",
+      url: "https://shopee.com.br/loja-qualidade",
+      image: "https://cf.shopee.sg/file/br-11134216-7r98s-lmvviyuk6fxz98",
+    },
+  ];
+  
